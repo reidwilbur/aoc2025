fc11ade91390676dab810c6613fc2891d7f1db22
Reid Wilbur <reid.wilbur@gmail.com>
2025-12-06 21:47:17 -0600

    Day6 sln2

--- src/main/java/com/wilb0t/aoc/Day6.java
+++ src/main/java/com/wilb0t/aoc/Day6.java
@@ -2,7 +2,6 @@ package com.wilb0t.aoc;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 
 class Day6 {
@@ -11,10 +10,11 @@ class Day6 {
     public static Input from(List<String> input) {
       var nums = new ArrayList<List<Integer>>();
       for (var line : input.subList(0, input.size() - 1)) {
-        var numline = Arrays.stream(line.split("\\s+"))
-            .filter(s -> !s.isEmpty())
-            .map(Integer::parseInt)
-            .toList();
+        var numline =
+            Arrays.stream(line.split("\\s+"))
+                .filter(s -> !s.isEmpty())
+                .map(Integer::parseInt)
+                .toList();
         nums.add(numline);
       }
       var ops = Arrays.stream(input.getLast().split("\\s+")).filter(s -> !s.isEmpty()).toList();
@@ -36,5 +36,38 @@ class Day6 {
       accum += probVal;
     }
     return accum;
+  }
+
+  public static long getProblemSumT(List<String> input) {
+    var accum = 0L;
+    var nums = new ArrayList<Integer>();
+    for (var col = input.getFirst().length() - 1; col >= 0; col--) {
+      var sb = new StringBuilder();
+      for (var row = 0; row < input.size() - 1; row++) {
+        sb.append(input.get(row).charAt(col));
+      }
+      var numstr = sb.toString().trim();
+      if (numstr.isBlank()) {
+        var op = input.getLast().charAt(col + 1);
+        accum += solveProb(nums, op);
+        nums.clear();
+      } else {
+        nums.add(Integer.parseInt(numstr));
+      }
+    }
+    var op = input.getLast().charAt(0);
+    accum += solveProb(nums, op);
+    return accum;
+  }
+
+  public static long solveProb(List<Integer> nums, char op) {
+    long probVal = nums.getFirst();
+    for (var idx = 1; idx < nums.size(); idx++) {
+      switch (op) {
+        case '+' -> probVal += nums.get(idx);
+        case '*' -> probVal *= nums.get(idx);
+      }
+    }
+    return probVal;
   }
 }
--- src/test/java/com/wilb0t/aoc/Day6Test.java
+++ src/test/java/com/wilb0t/aoc/Day6Test.java
@@ -2,20 +2,31 @@ package com.wilb0t.aoc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class Day6Test {
 
-  private static final Day6.Input TEST_INPUT = Input.TEST.parse(Day6.Input::from);
-  private static final Day6.Input PUZZLE_INPUT = Input.PUZZLE.parse(Day6.Input::from);
+  private static final List<String> TEST_INPUT = Input.TEST.loadStrings();
+  private static final List<String> PUZZLE_INPUT = Input.PUZZLE.loadStrings();
 
   @Test
   void testGetProblemSum_test() {
-    assertThat(Day6.getProblemSum(TEST_INPUT)).isEqualTo(4277556L);
+    assertThat(Day6.getProblemSum(Day6.Input.from(TEST_INPUT))).isEqualTo(4277556L);
   }
 
   @Test
   void testGetProblemSum_puzzle() {
-    assertThat(Day6.getProblemSum(PUZZLE_INPUT)).isEqualTo(5346286649122L);
+    assertThat(Day6.getProblemSum(Day6.Input.from(PUZZLE_INPUT))).isEqualTo(5346286649122L);
+  }
+
+  @Test
+  void testGetProblemSumT_test() {
+    assertThat(Day6.getProblemSumT(TEST_INPUT)).isEqualTo(3263827L);
+  }
+
+  @Test
+  void testGetProblemSumT_puzzle() {
+    assertThat(Day6.getProblemSumT(PUZZLE_INPUT)).isEqualTo(10389131401929L);
   }
 }
